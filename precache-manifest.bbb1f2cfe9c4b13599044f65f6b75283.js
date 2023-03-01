1029082a8cb7a4679fb43202a31cf8d62e604d5c
vishaldev89 <freelancervishal89@gmail.com>
2023-03-01 12:27:07 +0530

    Updates

---
+++ precache-manifest.bbb1f2cfe9c4b13599044f65f6b75283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15f5e4fd7c03662ea260",
+    "url": "/TextUtils-React/static/css/main.a132867a.chunk.css"
+  },
+  {
+    "revision": "15f5e4fd7c03662ea260",
+    "url": "/TextUtils-React/static/js/main.15f5e4fd.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/TextUtils-React/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "00c640c68c7208803f8f",
+    "url": "/TextUtils-React/static/js/2.00c640c6.chunk.js"
+  },
+  {
+    "revision": "c664369cb110a7f6239b",
+    "url": "/TextUtils-React/static/js/runtime~main.c664369c.js"
+  },
+  {
+    "revision": "3d295589c4deb31ded44b702cf5bf2e0",
+    "url": "/TextUtils-React/index.html"
+  }
+];
